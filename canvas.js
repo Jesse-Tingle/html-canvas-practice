28cd2bf1b58a064529402cd96e6da8c4ad84f5cb
Jesse-Tingle <jtingle0@gmail.com>
2021-10-26 18:09:51 -0400

    creates a line on canvas

--- canvas.js
+++ canvas.js
@@ -5,9 +5,17 @@ canvas.height = window.innerHeight
 
 var c = canvas.getContext("2d")
 
+//squares
 c.fillRect(100, 100, 100, 100)
 c.fillRect(300, 100, 100, 100)
 c.fillRect(500, 100, 100, 100)
 c.fillRect(700, 100, 100, 100)
 
 console.log(canvas)
+
+//line
+c.beginPath
+c.moveTo(150, 500)
+c.lineTo(300, 400)
+c.lineTo(400, 370)
+c.stroke()
