1449cdbf313c19ad096e90e193a6abb17c3dc5c0
Jesse-Tingle <jtingle0@gmail.com>
2021-10-26 18:30:33 -0400

    circles at random x, y and random colors

--- canvas.js
+++ canvas.js
@@ -21,7 +21,17 @@ c.lineTo(400, 370)
 c.stroke()
 
 //arc or circle
-c.beginPath()
-c.arc(300, 300, 30, 0, Math.PI * 2, false)
-c.strokeStyle = "blue"
-c.stroke()
+// c.beginPath()
+// c.arc(300, 300, 30, 0, Math.PI * 2, false)
+// c.strokeStyle = "blue"
+// c.stroke()
+
+for (var i = 0; i < 5; i++) {
+	var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16)
+	var x = Math.random() * window.innerWidth
+	var y = Math.random() * window.innerHeight
+	c.beginPath()
+	c.arc(x, y, 30, 0, Math.PI * 2, false)
+	c.fillStyle = randomColor
+	c.fill()
+}
